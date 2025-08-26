35011e43dfc336b0d37e1944a211ef4f4b4874b2
Falkort <lleclercq91@outlook.com>
2025-08-26 11:13:01 +0200

    Helper Slugify

--- app/routers/index.php
+++ app/routers/index.php
@@ -1,9 +1,13 @@
 <?php
 
+//Route posts.show:
+//URL: /posts/id/titre-du-post
+//PATTERN: ?posts=show&id=x
+
+
 //Route par défaut:
 //PATTERN: /
 //CTRL: postsController
 //Action: index
-
 include_once '../app/controllers/postsController.php';
     \App\Controllers\PostsController\indexAction($connection);
